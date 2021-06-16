8a700e41cb7ee616aa11cb5b15bd39abf6d1cad1
mehtaReena <j.reena07@gmail.com>
2021-06-16 13:09:29 +0530

    part1

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,44 @@
+
+import { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { signUp, signupErr } from "../redux/actions/userAction";
+import "../styles.css"
+function SignUp() {
+    const email = useRef();
+    const password = useRef();
+    const repassword = useRef();
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const {signupError, confirm} = useSelector(state => state.userState);
+
+    const clickhandler = () => {
+        if(password.current.value !== repassword.current.value) {
+            dispatch(signupErr('Password mismatch'));
+            return;
+        };
+
+        dispatch(signUp(email.current.value, password.current.value));
+    }
+
+    if(confirm) history.push("/");
+
+    return (
+        <div className="sign-up">
+            <h4>Create account.</h4>
+            <div>
+            <input type="email" ref={email} placeholder="email" />
+            <div>
+             <input type="password" ref={password} placeholder="password" />
+            <input type="password" ref={repassword} placeholder="re-enter password" />
+            </div>
+
+            </div>
+            <div>            <button onClick={clickhandler}>Submit</button></div>
+
+            {signupError ? <p>{signupError}</p> : null}
+        </div>
+    )
+}
+
+export default SignUp;
