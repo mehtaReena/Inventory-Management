a35937227481a2c7f1a62f341350a55a2d8cf1ca
mehtaReena <j.reena07@gmail.com>
2021-06-19 10:08:29 +0530

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,67 +1,27 @@
-
-
 import { useHistory } from "react-router";
 import Navbar from "./Navbar";
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchMobiles } from '../redux/actions/mobileAction';
-import ProductCard from './ProductCard'
 
 
 function App() {
-    const {validate} = useSelector(state => state.userState);
-
-    const history = useHistory();
-    let dispatch =useDispatch()
-
-   let mobileState= useSelector(state=>state.mobileState)
-   console.log("mobileState    " , mobileState)
-
+    let history = useHistory();
     const clickHandler = () => {
-        console.log( validate)
-        if(validate) {
-           history.push('/mobiles')
-
-        }
+        history.push("/signin");
     }
 
-    const getData=(category) =>{
-        dispatch(fetchMobiles(category))
-    }
 
     return (
         <>
 
             <div className="container">
-                  <Navbar/>
-                  <div className="display">
-                      <div className ="sidebar">
-
-                          <div><button  value="mobiles" onClick={(e) => getData(e.target.value)} >Mobiles</button></div>
-                          <div><button value ="laptops" onClick={(e) => getData(e.target.value)} >Laptops</button></div>
-                          <div><button  value ="appliances" onClick={(e) => getData(e.target.value)} >Appliances</button></div>
-
-                      </div>
-                      <div className="detail-container">
-                      <button className= "add" onClick={clickHandler}> Add Product</button>
-
-                {mobileState.loading ? (
-                    <h3>Loading . . .</h3>
-                ) : (
-                    <div className="details">
-                        {mobileState.mobiles.map((item) =>
-                            <ProductCard
-                                name={item.name}
-                                url={item.image}
-                                price={item.price}
-                            />
-
-                        )}
+                <Navbar />
+                <h1>Welcome to store</h1>
+                <div className="homePage">
+                    <div>
+                        <button className="button homebtn" onClick={clickHandler}> login here</button>
                     </div>
 
-                )}
+                </div>
             </div>
-                      </div>
-                  </div>
 
 
         </>
